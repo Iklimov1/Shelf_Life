296852d257182def28a1bc2d25d3e41cef323f0c
William Lampus <lampuswilliam@gmail.com>
2025-03-05 12:07:30 -0500

    Feat: Added Base Fragment and added simple functionality to change between them

--- app/src/main/java/com/example/shelflife/MainActivity.java
+++ app/src/main/java/com/example/shelflife/MainActivity.java
@@ -8,7 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomePage.WelcomeListner, My_Pantry.ItemListner,
+Add_Item.BackListner,Add_Item.ExperationListner,Calender.BackListner{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +21,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void GoToMy_Pantry() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView,new My_Pantry(),"main")
+                .addToBackStack(null)
+                .commit();
+
+    }
+
+    @Override
+    public void GoToAdd_item() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView,new Add_Item(),"Adding Item")
+                .addToBackStack(null)
+                .commit();
+
+    }
+
+    @Override
+    public void Back() {
+        getSupportFragmentManager().popBackStack();
+
+    }
+
+    @Override
+    public void GoToCalender() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView,new Calender())
+                .addToBackStack(null)
+                .commit();
+
     }
 }
